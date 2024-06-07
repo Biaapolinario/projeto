0eefcac0cf0d8be482c6be30f5bf2c7fd27b2992
Biaapolinario <beatrizapolinario@gmail.com>
2024-06-07 10:40:49 -0300

    Atualização...

--- js/forte.js
+++ js/forte.js
@@ -1,11 +1,8 @@
 //aplicação da funcionalidade do javascript
 document.querySelectorAll('button').forEach(button => { 
     button.addEventListener('click' , () => {
-})
-
-    //o botão adiciona um evento do mouse (click)
-    button.addEventListener('click' , () => {
         document.body.className = button.id
     })
-
 })
+
+
